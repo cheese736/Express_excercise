3a5233b8ad5b72b7a7c2e6161cf02fa8a44522aa
Aither <cheese736@hotmail.com>
2022-09-30 03:04:47 +0800

    init

---
+++ app.js
@@ -0,0 +1,41 @@
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+const movieList = require('./movies.json')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+// routes setting
+app.get('/', (req, res) => {
+  // past the movie data into 'index' partial template
+  res.render('index', { movies: movieList.results })
+})
+
+// app.get('/movies/1', (req, res) => {
+//     const movieOne = {
+//       id: 1,
+//       title: 'Jurassic World: Fallen Kingdom',
+//       description:
+//         'Several years after the demise of Jurassic World, a volcanic eruption threatens the remaining dinosaurs on the island of Isla Nublar. Claire Dearing, the former park manager and founder of the Dinosaur Protection Group, recruits Owen Grady to help prevent the extinction of the dinosaurs once again.',
+//       release_date: '2018-06-06',
+//       image: 'c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg'
+//     }
+//     res.render('show', { movie: movieOne })
+//   })
+
+app.get('/movies/:movie_id', (req, res) => {
+   const movie = movieList.results.find(movie => movie.id.toString() === req.params.movie_id)
+  res.render('show', { movie: movie })
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
